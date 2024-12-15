c237a751fe2bf230c2c6ab1444df92452b4c8d0a
flavio0001 <flaviosantosofmusic@gmail.com>
2024-12-14 21:51:06 -0300

    added number of attempts

--- exercise_list/guess_the_number/Main.java
+++ exercise_list/guess_the_number/Main.java
@@ -7,16 +7,18 @@ public class Main {
         Random random = new Random();
         int randomNumber = random.nextInt(100) + 1;
 
-        System.out.println("Welcome to guess the number!");
+        System.out.println("Welcome to Guess the Number!");
         System.out.println("I have picked a number between 1 and 100. Try to guess it!");
 
         int userGuess = 0;
+        int attempts = 0;
 
         do {
             System.out.println("Enter your guess:");
 
             if (input.hasNextInt()) {
                 userGuess = input.nextInt();
+                attempts++;
 
                 if (userGuess < randomNumber) {
                     System.out.println("The number is higher.");
@@ -24,9 +26,9 @@ public class Main {
                     System.out.println("The number is lower.");
                 } else {
                     System.out.println("Congratulations! You guessed it!");
+                    System.out.println("You needed " + attempts + " attempts.");
                     break;
                 }
-
             } else {
                 System.out.println("Invalid input. Please enter a number.");
                 input.next();

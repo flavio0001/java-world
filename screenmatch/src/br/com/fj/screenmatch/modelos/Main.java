82a93d8746b01d87cd286611eaf53772b0967c81
flavio0001 <flaviosantosofmusic@gmail.com>
2024-12-18 04:54:37 -0300

    criando e praticando os conhecimentos de POO (Polimorfismo e orientação a objetos)

---
+++ screenmatch/src/br/com/fj/screenmatch/modelos/Main.java
@@ -0,0 +1,45 @@
+package br.com.fj.screenmatch.modelos;
+
+import br.com.fj.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.fj.screenmatch.modelos.Filme;
+import br.com.fj.screenmatch.modelos.Serie;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        //System.out.println(meuFilme.pegaMedia());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar lost: " + lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+    }
+}
